746bb0908679fa60e93e69827df0125bdc728cd4
botkumar01 <harishff2020@gmail.com>
2025-08-20 15:52:31 +0530

    t

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -21,7 +21,7 @@ const Nav: React.FC<{ items: NavItem[] }> = ({ items }) => {
     <>
     <div className="platformIcon">
           <img src={siteIcon} alt="Platform Icon" />
-          <span>PodoPosts</span>
+          <span>PodtoPosts</span>
         </div>
       <nav className="siteNav">
         
